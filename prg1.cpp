63e1ff5edc62cdaef4b9dee8462f30ff9ee3fc84
harshit <126167058+harshit6978@users.noreply.github.com>
2023-06-04 20:59:12 +0530

    Add files via upload

---
+++ prg1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class house{
+
+	public:	
+	int id;
+	char name[20];
+	
+};
+
+int main(){
+	
+	house obj;
+	
+	obj.id=101;
+	cout<<obj.id;
+	cout<<"someshngar\n";
+	
+	obj.id=102;
+	cout<<obj.id;
+	cout<<"jamnanagar\n";
+	
+	obj.id=103;
+	cout<<obj.id;
+	cout<<"dvarkeshnagar\n";
+	
+	obj.id=104;
+	cout<<obj.id;
+	cout<<"sundaram\n";
+	
+		
+	obj.id=105;
+	cout<<obj.id;
+	cout<<"yogi soc\n";
+	
+	return 0;
+}
---
+++ prg4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+ 
+ using namespace std;
+ class car{
+ 
+ 	public:
+ 		int id;
+ 		char name[20];
+ 
+ };
+ 
+ int main(){
+ 	
+ 
+ 	car obj;
+ 	
+ 	obj.id=111;
+ 	cout<<obj.id;
+ 	cout<<"suzuki\t";
+ 	cout<<"red\t";
+ 	
+ 	
+ 	return 0;
+ }
