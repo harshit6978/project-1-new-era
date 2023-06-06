abec8066882d7fe76e45d859bad2fc917b074fc5
harshit <126167058+harshit6978@users.noreply.github.com>
2023-06-06 21:07:24 +0530

    Update prg4.cpp

--- prg4.cpp
+++ prg4.cpp
@@ -1,24 +1,74 @@
 #include<iostream>
- 
- using namespace std;
- class car{
- 
- 	public:
- 		int id;
- 		char name[20];
- 
- };
- 
- int main(){
- 	
- 
- 	car obj;
- 	
- 	obj.id=111;
- 	cout<<obj.id;
- 	cout<<"suzuki\t";
- 	cout<<"red\t";
- 	
- 	
- 	return 0;
- }
+#include<string.h>
+
+using namespace std;
+
+class employe{
+	
+	public:
+		int id,model,year;
+		char name[20],color[20];
+};
+
+int main(){
+	
+	employe obj,obj2,obj3,obj4;
+	
+	cout<<"first car\n\n";
+	obj.id=1;
+	obj.model=2015;
+	obj.year=2008;
+	strcpy(obj.name,"audi");
+	strcpy(obj.color,"grey");
+	
+	cout<<obj.id<<"\n";
+	cout<<obj.model<<"\n";
+	cout<<obj.year<<"\n";
+	cout<<obj.name<<"\n";
+	cout<<obj.color<<"\n\n\n";
+
+	
+
+	cout<<"second car\n\n";
+	obj.id=2;
+	obj.model=2012;
+	obj.year=2002;
+	strcpy(obj.name,"suzuki");
+	strcpy(obj.color,"black");
+	
+	cout<<obj.id<<"\n";
+	cout<<obj.model<<"\n";
+	cout<<obj.year<<"\n";
+	cout<<obj.name<<"\n";
+	cout<<obj.color<<"\n\n\n";
+	
+	
+	cout<<"third car\n\n";
+	obj.id=3;
+	obj.model=2021;
+	obj.year=2001;
+	strcpy(obj.name,"mg");
+	strcpy(obj.color,"blacy");
+	
+	cout<<obj.id<<"\n";
+	cout<<obj.model<<"\n";
+	cout<<obj.year<<"\n";
+	cout<<obj.name<<"\n";
+	cout<<obj.color<<"\n\n\n";
+	
+	
+	cout<<"four car\n\n";
+	obj.id=4;
+	obj.model=2022;
+	obj.year=2000;
+	strcpy(obj.name,"kia");
+	strcpy(obj.color,"silver");
+	
+	cout<<obj.id<<"\n";
+	cout<<obj.model<<"\n";
+	cout<<obj.year<<"\n";
+	cout<<obj.name<<"\n";
+	cout<<obj.color<<"\n\n\n";
+	
+
+}
